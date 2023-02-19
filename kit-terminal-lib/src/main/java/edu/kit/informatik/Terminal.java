1f2146ccb6761f7db9e7699d183c918fd832b14d
Max Schweikart <hello@maxschweik.art>
2023-02-19 15:57:56 +0100

    docs: add documentation and suppress warnings in terminal reimplementation

--- kit-terminal-lib/src/main/java/edu/kit/informatik/Terminal.java
+++ kit-terminal-lib/src/main/java/edu/kit/informatik/Terminal.java
@@ -4,15 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * This is a re-implementation of the terminal class we had to use for interacting with
+ * {@link System#in}, {@link System#out} and {@link System#err}.
+ * The implementation is probably incomplete and was reconstructed only from the method signatures
+ * used in my code.
+ * It might be erroneous and has not been testet properly as it was only implemented to get my
+ * code to run again.
+ */
 public class Terminal {
   private static final BufferedReader INPUT_READER =
       new BufferedReader((new InputStreamReader(System.in)));
 
+  private Terminal() {
+    throw new UnsupportedOperationException("This utility class should not be instantiated!");
+  }
+
+  @SuppressWarnings("java:S106") // for using System.err
   public static void printError(String message) {
+    // original implementation printed to System.err too
     System.err.println(message);
   }
 
+  @SuppressWarnings("java:S106") // for using System.out
   public static void printLine(String message) {
+    // original implementation printed to System.out too
     System.out.println(message);
   }
 
@@ -20,10 +36,12 @@ public class Terminal {
     printLine(String.valueOf(id));
   }
 
+  @SuppressWarnings("java:S112") // for rethrowing as runtime exception
   public static String readLine() {
     try {
       return INPUT_READER.readLine();
     } catch (IOException e) {
+      // original implementation made the program crash too
       throw new RuntimeException("Could not read input", e);
     }
   }
